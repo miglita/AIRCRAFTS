e3dfc3779c726c0d92c5649e98681337a8fd356e
miglita <miglelevinskaite@gmail.com>
2019-04-05 07:38:40 +0300

    Comments added & minor changes made

--- table.py
+++ table.py
@@ -7,32 +7,37 @@ from email import encoders
 import os.path
 import csv
 
+"""
+This code reads csv files, extracts row information from them, adds row information to HTML code
+and sends an HTML email with 2 tables.
+"""
+# Below, within HTML tags, loop goes through rows in csv file. Row is a variable for row and strRW is row + html tags for formatting
+#--------------------------------------------------------------------
+# First HTML table
+#--------------------------------------------------------------------
 html = "<html><table bgcolor=#ADD8E6><tr><th>AIRCRAFTS EU</th></tr>"
 with open('EU_CNTRS.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     line_count = 0
     for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
         row = (f'{row["TAIL_NUMBER"]} , {row["MODEL_NUMBER"]} , {row["DESCRIPTION"]}, {row["COMPANY_NAME"]}, {row["CODE"]}, {row["COUNTRY_NAME"]}.')
         line_count += 1
         strRW = "<tr><td><hr>" + str(row) + "</hr></td></tr>"
         html = html + strRW
-
 html = html + "</table></html>"
+
+# html variable is updated with row information and closing tags are added, new html file (below) is created
 
 hs = open("table1foremail.html", 'w')
 hs.write(html)
-#--------------------------------------------------------------------------------------
+#--------------------------------------------------------------------
+# Second HTML table
+#--------------------------------------------------------------------
 html1 = "<html><table bgcolor=#FFA07A><tr><th>AIRCRAFTS NON EU</th></tr>"
 with open('NON_EU_CNTRS.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     line_count = 0
     for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
         row = (f'{row["TAIL_NUMBER"]} , {row["MODEL_NUMBER"]} , {row["DESCRIPTION"]}, {row["COMPANY_NAME"]}, {row["CODE"]}, {row["COUNTRY_NAME"]}.')
         line_count += 1
         strRW1 = "<tr><td><hr>" + str(row) + "</hr></td></tr>"
@@ -41,18 +46,25 @@ with open('NON_EU_CNTRS.csv', mode='r') as csv_file:
 html1 = html1 + "</table></html>"
 hs = open("table1foremail1.html", 'w')
 hs.write(html1)
-#---------------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------
+# Send email part
+#--------------------------------------------------------------------
+
+# Declaring variables
+
 email = 'miglelevinskaite@gmail.com'
-password = 'psswd'
+password = 'pssw'
 send_to_email = 'miglelevinskaite@gmail.com'
 subject = 'Information about Aircrafts'
-message = 'Please see tables below:'
+
+# MIMEMultipart needed to send HTML as object
 
 msg = MIMEMultipart()
 msg['From'] = email
 msg['To'] = send_to_email
 msg['Subject'] = subject
-# -----------------------------------------------------------------------------
+
 msg.attach(MIMEText(html, 'html'))
 msg.attach(MIMEText(html1, 'html'))
 
